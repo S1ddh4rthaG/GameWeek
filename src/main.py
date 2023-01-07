64db3242e05d35f3f47fb542228ed0c84c7ec551
Mathew K J <79681351+MathewKJ2048@users.noreply.github.com>
2023-01-07 11:56:32 +0530

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -1,43 +1,40 @@
-import random
-
 import pygame
+import random
 import math
 
-# import random
-
-pygame.init()
-
-height = 720
+height = 800
 width = 1200
 max_frame_rate = 60
-movable_width = 800
-movable_height = 500
+movable_width = 600
+movable_height = 360
+world_size = 4000
 
+pygame.init()
 screen = pygame.display.set_mode((width, height))
-
 pygame.display.set_caption("Infinite World")
 icon = pygame.image.load("assets/icon.png")
 pygame.display.set_icon(icon)
 
 
-class Object:
+class Object:  # This is the base class for all elements in the game
     x = 0
-    y = 0
-    v = 0
-    theta = 0
-    w = 0
+    y = 0       # co-ordinates in absolute space
+    v = 0       # magnitude of velocity vector
+    theta = 0   # angle made with positive x axis
+    w = 0       # angular velocity
     is_valid = True
-    sprite = None
+    sprite = None   # image to display at the location of the object
 
-    def step(self, dt):
+    def step(self, dt):     # moves the object in one frame
         v_x = math.cos(math.radians(self.theta)) * self.v
         v_y = math.sin(math.radians(self.theta)) * self.v
         self.x += v_x * dt
         self.y += v_y * dt
         self.theta += self.w * dt
+        self.theta %= 360
 
 
-class Bullet(Object):
+class Fireball(Object):   # shot by player using mouse
     velocity = 1
     sprite = pygame.image.load("./assets/bullet.png")
     timer = 1000
@@ -52,16 +49,16 @@ class Bullet(Object):
         self.theta = math.degrees(math.atan2(m_y - source.y, m_x - source.x))
 
 
-class Player(Object):
-    max_v = 0.4
-    max_w = 0.1
+class Player(Object):   # controlled by player
+    max_v = 0.4         # running velocity
+    max_w = 0.1         # turning angular velocity
     sprite = pygame.image.load("./assets/player.png")
 
-    def __process_event__(self, e, objs):
+    def __process_event__(self, e, objs):   # processes keystrokes
         if e.type == pygame.MOUSEBUTTONDOWN:
-            objs.append(Bullet(self))
+            objs.append(Fireball(self))
 
-    def __process_input__(self):
+    def __process_input__(self):            # makes state of player reflect state of keyboard
         key_list = pygame.key.get_pressed()
         if key_list[pygame.K_w] and key_list[pygame.K_s]:
             self.v = 0
@@ -81,11 +78,12 @@ class Player(Object):
             self.w = 0
 
 
+# display details
 x_start = 0
 y_start = 0
 
 
-def map(objs, map_scale, expansion_scale, x, y):
+def map(objs, map_scale, expansion_scale, x, y):  # shows minimap
     global x_start
     global y_start
     dot_r = pygame.image.load("assets/dot_red.png")
@@ -98,11 +96,11 @@ def map(objs, map_scale, expansion_scale, x, y):
                                   y + map_scale * (expansion_scale - 1) * height / 2),
                                  (map_scale * width, map_scale * height)), 1)
     for o in objs:
-        if type(o) == Bullet:
+        if type(o) == Fireball:
             continue
         x_ = o.x - x_start + (expansion_scale - 1) * width / 2
         y_ = o.y - y_start + (expansion_scale - 1) * height / 2
-        if 0 <= x_ <= expansion_scale * width and 0 <= y_ <= expansion_scale * height or True:
+        if 0 <= x_ <= expansion_scale * width and 0 <= y_ <= expansion_scale * height:
             x__ = map_scale * x_
             y__ = map_scale * y_
             dot = dot_g if type(o) == Player else dot_r
@@ -137,27 +135,31 @@ def display(objs):
             screen.blit(img, (_X, _Y))
 
 
-objects = list()
+objects = list()    # this list contains all elements of the game
 
-for i in range(1000):
+for i in range(int(world_size*world_size/100000)):
     o = Object()
     o.sprite = pygame.image.load("./assets/object.png")
-    o.x = (random.random() - 0.5) * width * 10
-    o.y = (random.random() - 0.5) * height * 10
+    o.x = (random.random()-0.5) * world_size
+    o.y = (random.random()-0.5) * world_size
     objects.append(o)
 
 p = Player()
 p.x = width / 2
 p.y = height / 2
 objects.append(p)
-c = pygame.time.Clock()
+c = pygame.time.Clock()     # used to track time in milliseconds
 running = True
+map_toggle = True   # decides whether the map is shown or not
 while running:
 
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_m:
+                map_toggle = not map_toggle
         p.__process_event__(event, objects)
 
     p.__process_input__()
@@ -169,7 +171,8 @@ while running:
         o.step(dt)
     update_window(p.x, p.y)
     display(objects)
-    map(objects, 0.1, 2, 40, 40)
+    if map_toggle:
+        map(objects, 0.1, 2, 40, 40)
     pygame.display.update()
 
     pass
